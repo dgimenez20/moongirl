a41eeda7cd74ce039842b22ccc362caa134dc061
dgimenez20 <158026468+dgimenez20@users.noreply.github.com>
2024-02-21 08:45:05 +0100

    Update moongirl.sh

--- moongirl.sh
+++ moongirl.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+#Trampa de crl+C, cada cop que el usuari faci un crl+C el programa indicara que per sorti necesites introduir el numero 7. 
 trap 'echo Per sortir de programa has de posar el numero 7' SIGINT
 
+#Funció de menu
 function menu(){
 	echo "--------------------"
         echo "| Escull opcio:    |"
@@ -17,24 +19,32 @@ function menu(){
         read -p "" opcio
 }
 
-
+#Fins que no hi hagi conectivitat el codi mostrara error de conexió
 until ping -c 5 '192.168.1.1';
 do
   echo "ERROR DE CONEXIO"
   sleep 5
 done
 
+#variable per sortir del bucle
 sortir=0
+
 
 while [ $sortir != 1  ]
 do
-	menu
+#crido la funció del menu cada volta
+ 	menu
 
+#faig un case de totes les opcions de menu
 	case $opcio in
 		'1')
+  			#em conecto mitjançant un ssh
 			ssh -T root@192.168.1.1 <<- EOF
+   			#redirigeixo la comanda a un fitxer
 			lshw -short>lshw.txt;
+   			#mostro el fitxer amb un cat
 			cat lshw.txt
+   			#finalment faig un scp del fitxer i m'ho paso a la maquina local
 			scp lshw.txt david@192.168.1.10:/home/david/resultats.txt
 			EOF
 		;;
@@ -55,6 +65,7 @@ do
 		;;
 		'4')
 			ssh -T root@192.168.1.1 <<- EOF
+   			#instalo el net-tools en cas de que no esitgues instalat
 			apt install net-tools
 			netstat>netstat.txt
 			cat netstat.txt
@@ -76,23 +87,31 @@ do
 			EOF
                 ;;
 		'7')
+  			#igualo la variable de sortida a 1 i el bucle acaba
 			sortir=1
 		;;
 	esac
+ 	#faig un if per que no entri en cas de finalització del programa
 	if [ $opcio != '7' ];then
+ 		#creo una variable de data
 		data=`date +%F`
+  		#creo el index.html
 		cat <<- FITXER > index.html
-		<html>
+		#creo el HTML per mostra-lo en el navegador
+  		<html>
 			<head><title>Resultats</title></head>
 			<body>
 				<h1>Resultats:</h1>
+    				#introdueixo la variable data en un h3
 				<h3>$data</h3>
 				<pre>
 		FITXER
+  		#faig un cat del fitxer on he guardat els resultats de la comanda 
 		cat /home/david/resultats.txt >> index.html
 		echo "</pre>" >> index.html
 		echo "</body>" >> index.html
 		echo "</html>" >> index.html
+  		#finalment mostro el resultat del index.html en el navegador
 		firefox index.html
 	fi
 done
